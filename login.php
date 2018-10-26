8f3806b4950fc592f3223f10d5859b7c389ec114
Raymon37 <43744360+Raymon37@users.noreply.github.com>
2018-10-26 15:27:02 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,36 @@
+<?php include('server.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="header">
+        <h2>Login</h2>
+    </div>
+    
+    <form method="post" action="login.php">
+        <!-- displah validation errors here -->
+        <?php include('errors.php'); ?>
+        <div class="input-group">
+            <label>Username</label>
+            <input type="text" name="username">
+        </div>
+        <div class="input-group">
+            <label>Password</label>
+            <input type="text" name="password">
+        </div>
+        <div class="input-group">
+            <button type="submit" name="login" class="btn">Login</button>
+        </div>
+        <p>
+            Not yet a member? <a href="register.php">Sign up</a>
+        </p>
+    </form>
+
+</body>
+</html>
